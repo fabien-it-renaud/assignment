41720ecc63d4bd002611fe6a365d1bd9df43f574
renaud <renaud@dell-fab.saclay.inria.fr>
2014-12-15 09:13:20 +0100

    Some cleanup

--- test/searchengine/DocumentTest.java
+++ test/searchengine/DocumentTest.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package searchengine;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.List;
-import java.util.ArrayList;
 
 /**
  *
--- test/searchengine/SearchEngineTest.java
+++ test/searchengine/SearchEngineTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package searchengine;
 
 import java.util.ArrayList;
@@ -10,10 +5,7 @@ import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author renaud
- */
+
 public class SearchEngineTest {
 
     /**
@@ -65,7 +57,7 @@ public class SearchEngineTest {
     }
     
     /**
-     * Test searching the empty string
+     * Test searching a string which is not contained in any document
      */
     @Test
     public void testAbsbsentString() {
